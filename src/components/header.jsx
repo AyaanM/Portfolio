dcadbfcafb6bf2f539cacb4065921ccf1575555c
AyaanM <ayaan.merchant08@gmail.com>
2025-06-06 17:05:55 -0600

    fixed tailwind

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,18 @@
 const Header = () => {
     return (
-      <div>Header</div>
+      <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+        <div className="max-w-screen-2xl w-full mx-auto px-4">
+          <h1>
+            <a href="/logo.png" className="logo">
+            <img src="/logo.png" width={40} height={40} alt="AM" />
+            </a>
+          </h1>
+
+          <div className="">
+            b
+          </div>
+        </div>
+      </header>
     )
   }
 
