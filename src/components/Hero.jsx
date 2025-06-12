6713d8330bfae5edfab5b3f37f545c0dec16cf80
AyaanM <ayaan.merchant08@gmail.com>
2025-06-12 17:59:00 -0600

    changes to coffee button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,13 +11,13 @@ const Hero = () => {
                 <h1 className="headline-1 my-3">Ayaan Merchant</h1>
                 <p className="font-light my-3">Web Developer</p>
 
-                <a href="#coffee" className="inline-block px-6 py-3 my-3 ring-cyan text-cyan-400 rounded">
-                    Let's Grab a Virtual Coffee</a>
+                <a href="#coffee" className="coffee-btn">
+                    Let's Grab a Virtual Coffee <span className="material-symbols-outlined animate-bounce">local_cafe</span></a>
 
                 <div className="container flex gap-15 justify-center items-center my-3">
-                    <div className="w-8"><a href="https://github.com/AyaanM" target="_blank"><img src="/Portfolio/github_logo_white.png" alt="GitHub"/></a></div>
-                    <div className="w-8"><a href="https://www.linkedin.com/in/ayaanm1/" target="_blank"><img src="/Portfolio/link_logo_white.png" className="w-full h-full object-contain" alt="LinkedIn"/></a></div>
-                    <div className="w-8"><a href="mailto:ayaan.merchant08@gmail.com" target="_blank"><span className="material-symbols-outlined" style={{fontSize: '35px'}}>mail</span></a></div>
+                    <div className="socials"><a href="https://github.com/AyaanM" target="_blank"><img src="/Portfolio/github_logo_white.png" alt="GitHub"/></a></div>
+                    <div className="socials"><a href="https://www.linkedin.com/in/ayaanm1/" target="_blank"><img src="/Portfolio/link_logo_white.png" className="w-full h-full object-contain" alt="LinkedIn"/></a></div>
+                    <div className="socials"><a href="mailto:ayaan.merchant08@gmail.com" target="_blank"><span className="material-symbols-outlined" style={{fontSize: '35px'}}>mail</span></a></div>
                 </div>
             </div>
 
