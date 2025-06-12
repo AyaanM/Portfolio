eb81a7476f9252edc32be742b77a27b916aec727
AyaanM <ayaan.merchant08@gmail.com>
2025-06-12 17:34:36 -0600

    links work well

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,9 +15,9 @@ const Hero = () => {
                     Let's Grab a Virtual Coffee</a>
 
                 <div className="container flex gap-15 justify-center items-center my-3">
-                    <div className="w-8"><img src="/Portfolio/github_logo_white.png" alt="GitHub"/></div>
-                    <div className="w-8"><img src="/Portfolio/link_logo_white.png" className="w-full h-full object-contain" alt="LinkedIn"/></div>
-                    <div className="w-8 flex justify-center"><span className="material-symbols-outlined" style={{fontSize: '35px'}}>mail</span></div>
+                    <div className="w-8"><a href="https://github.com/AyaanM" target="_blank"><img src="/Portfolio/github_logo_white.png" alt="GitHub"/></a></div>
+                    <div className="w-8"><a href="https://www.linkedin.com/in/ayaanm1/" target="_blank"><img src="/Portfolio/link_logo_white.png" className="w-full h-full object-contain" alt="LinkedIn"/></a></div>
+                    <div className="w-8"><a href="mailto:ayaan.merchant08@gmail.com" target="_blank"><span className="material-symbols-outlined" style={{fontSize: '35px'}}>mail</span></a></div>
                 </div>
             </div>
 
